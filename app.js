f0e76440355f7c47ef83aa62d98f91848cc00fc5
Ziaur Rab <rabziaur77@outlook.com>
2022-03-06 14:32:20 +0530

    Upload image and get text

--- app.js
+++ app.js
@@ -69,8 +69,10 @@ app.use(function(err, req, res, next) {
 
 var port = process.env.PORT || 3000
 
-app.listen(port, function(){
+var server = app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
+
+server.timeout = 0;
 
 module.exports = app;
